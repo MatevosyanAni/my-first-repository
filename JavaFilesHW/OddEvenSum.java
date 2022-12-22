53e5b899d33a2499e42a2d066fe1d09e6e60dc3d
Ani <ani.matevosyan.0202@mail.ru>
2022-12-23 02:07:23 +0400

    Add new created java files

---
+++ JavaFilesHW/OddEvenSum.java
@@ -0,0 +1,11 @@
+class OddEvenSum {  
+   public static void main(String[] args) {
+      // Declare variables
+      final int LOWERBOUND = 1;
+      final int UPPERBOUND = 1000;  
+      int sumOdd  = 0;    
+      int sumEven = 0;    
+      int absDiff; 
+System.out.println("The sum of odd numbers from " + LOWERBOUND + " to " + UPPERBOUND + " is: " + sumOdd);
+ }
+}
